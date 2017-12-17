b9e68c5aeab86d7591165a77edbf5e56dc825337
todoroki <s18.todoroki+git@gmail.com>
2017-12-17 19:07:17 +0900

    parse appstore review xml

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Author struct {
+	Name string `xml:"name"`
+	URI  string `xml:"uri"`
+}
+
+type Link struct {
+	Rel  string `xml:"rel,attr"`
+	Href string `xml:"href,attr"`
+}
+
+type Comment struct {
+	Type string `xml:"type,attr"`
+	Text string `xml:",chardata"`
+}
+
+type XML struct {
+	Reviews []struct {
+		Id      string    `xml:"id"`
+		Updated string    `xml:"updated"`
+		Title   string    `xml:"title"`
+		Comment []Comment `xml:"content"`
+		Rating  int       `xml:"rating"`
+		Version string    `xml:"version"`
+		Author  Author    `xml:"author"`
+		Link    Link      `xml:"link"`
+	} `xml:"entry"`
+}
+
+func main() {
+	storeURL := "https://itunes.apple.com/jp/rss/customerreviews/id=" + os.Getenv("APPSTORE_ID") + "/xml"
+	data := httpGet(storeURL)
+
+	result := XML{}
+	err := xml.Unmarshal([]byte(data), &result)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	for _, review := range result.Reviews {
+		fmt.Printf("%v\n", review.Id)
+		fmt.Printf("%v\n", review.Title)
+		fmt.Printf("%v\n", review.Updated)
+		fmt.Printf("%v\n", review.Comment[0].Text)
+		fmt.Printf("%d\n", review.Rating)
+		fmt.Printf("%v\n", review.Version)
+		fmt.Printf("%v\n", review.Author.Name)
+		fmt.Println()
+	}
+}
+
+func httpGet(url string) string {
+	response, _ := http.Get(url)
+	body, _ := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	return string(body)
+}
