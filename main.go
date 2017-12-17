8c85cb1edc6bc720af216616116da8cc654eb862
todoroki <s18.todoroki+git@gmail.com>
2017-12-17 23:24:32 +0900

    post reviews to slack

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 
+	"./slack"
 	"./storeReview"
 )
+
+const location = "Asia/Tokyo"
 
 func main() {
 	storeURL := "https://itunes.apple.com/jp/rss/customerreviews/id=" + os.Getenv("APPSTORE_ID") + "/xml"
@@ -16,20 +22,46 @@ func main() {
 	result := storeReview.XML{}
 	err := xml.Unmarshal([]byte(data), &result)
 	if err != nil {
-		fmt.Printf("error: %v", err)
+		log.Fatal(err)
 		return
 	}
 
+	webhookUrl := os.Getenv("REVIEW_SLACK_WEBHOOK_URL")
 	for _, review := range result.Reviews {
-		fmt.Printf("%v\n", review.Id)
-		fmt.Printf("%v\n", review.Title)
-		fmt.Printf("%v\n", review.Updated)
-		fmt.Printf("%v\n", review.Comment[0].Text)
-		fmt.Printf("%d\n", review.Rating)
-		fmt.Printf("%v\n", review.Version)
-		fmt.Printf("%v\n", review.Author.Name)
-		fmt.Println()
+		attachment := slack.Attachment{}
+		version := review.Version
+		star := strings.Repeat("⭐", int(review.Rating))
+		title := review.Title
+		comment := review.Comment[0].Text
+		author := review.Author.Name
+
+		updatedAt, err := time.Parse(
+			"2006-01-02T15:04:05-07:00",
+			review.Updated)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		loc, timeErr := time.LoadLocation(location)
+		if timeErr != nil {
+			log.Fatal(timeErr)
+		}
+		updatedStr := updatedAt.In(loc).Format("2006/01/02 Mon 15:04:05")
+
+		attachment.AddField(slack.Field { Title: star + " " + title, Value: comment })
+		payload := slack.Payload {
+			Text: "App Store Review\n" + "ver: " + version + " [" + updatedStr + "]",
+			Username: author,
+			Channel: os.Getenv("REVIEW_SLACK_CHANNEL"),
+			IconEmoji: os.Getenv("REVIEW_SLACK_ICON"),
+			Attachments: []slack.Attachment{attachment},
+		}
+		slackErr := slack.Send(webhookUrl, payload)
+		if len(slackErr) > 0 {
+			log.Fatal(slackErr)
+		}
 	}
+
 }
 
 func httpGet(url string) string {
---
+++ slack/slack.go
@@ -0,0 +1,78 @@
+package slack
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"encoding/json"
+)
+
+type Field struct {
+	Title string `json:"title"`
+	Value string `json:"value"`
+	Short bool   `json:"short"`
+}
+
+type Attachment struct {
+	Fallback   *string   `json:"fallback"`
+	Color      *string   `json:"color"`
+	PreText    *string   `json:"pretext"`
+	AuthorName *string   `json:"author_name"`
+	AuthorLink *string   `json:"author_link"`
+	AuthorIcon *string   `json:"author_icon"`
+	Title      *string   `json:"title"`
+	TitleLink  *string   `json:"title_link"`
+	Text       *string   `json:"text"`
+	ImageUrl   *string   `json:"image_url"`
+	Fields     []*Field  `json:"fields"`
+	Footer     *string   `json:"footer"`
+	FooterIcon *string   `json:"footer_icon"`
+	Timestamp  *int64    `json:"ts"`
+	MarkdownIn *[]string `json:"mrkdwn_in"`
+}
+
+type Payload struct {
+	Parse       string       `json:"parse,omitempty"`
+	Username    string       `json:"username,omitempty"`
+	IconUrl     string       `json:"icon_url,omitempty"`
+	IconEmoji   string       `json:"icon_emoji,omitempty"`
+	Channel     string       `json:"channel,omitempty"`
+	Text        string       `json:"text,omitempty"`
+	LinkNames   string       `json:"link_names,omitempty"`
+	Attachments []Attachment `json:"attachments,omitempty"`
+	UnfurlLinks bool         `json:"unfurl_links,omitempty"`
+	UnfurlMedia bool         `json:"unfurl_media,omitempty"`
+}
+
+func (attachment *Attachment) AddField(field Field) *Attachment {
+	attachment.Fields = append(attachment.Fields, &field)
+	return attachment
+}
+
+func Send(webhookUrl string, payload Payload) []error {
+	jsonBytes, err := json.Marshal(payload)
+	req, err := http.NewRequest(
+		"POST",
+		webhookUrl,
+		bytes.NewBuffer([]byte(string(jsonBytes))),
+	)
+
+	if err != nil {
+		fmt.Print(err)
+		return []error{fmt.Errorf("error: %v", err)}
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err)
+		return []error{fmt.Errorf("error: %v", err)}
+	}
+
+	fmt.Print(resp)
+	defer resp.Body.Close()
+
+	return nil
+}
